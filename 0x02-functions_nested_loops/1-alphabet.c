111c426ba7291b7105a571d9915e9ec155fb92c6
K-Lenin279 <leninkagema72@gmail.com>
2023-02-21 16:54:34 +0300

    Task alphabettt betty doc fix 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 #include "main.h"
 /**
  * main - Entry point
- *
+ * print_alphabet() - prints the alphabet, in lowercase, followed by a new line.
  * Return: (0) (Success)
  */
 void print_alphabet(void)
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 #include "main.h"
 /**
  * main - Entry point
- *
+ * print_alphabet_x10() - prints 10 times the alphabet, in lowercase, followed by a new line.
  * Return: (0) (Success)
  */
 void print_alphabet_x10(void)
