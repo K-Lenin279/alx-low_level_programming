8a3ee81151289432f011b81d488690268c261075
K-Lenin279 <leninkagema72@gmail.com>
2023-02-22 17:00:24 +0300

    Task 100 attempt

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,2 +1,11 @@
 #include <stdio.h>
 #include "main.h"
+
+/**
+ * print_times_table - prints the n times table, starting with 0
+ *
+ * @n: the int to check
+ *
+ * Return: return nothing
+ */
+void print_times_table(int n)
