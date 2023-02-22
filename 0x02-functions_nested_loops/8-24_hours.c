45cbb184842a88f75055d1ae6a5c0ab1c4e8f92f
K-Lenin279 <leninkagema72@gmail.com>
2023-02-22 15:26:55 +0300

    Task jack_s day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * starting from 00:00 to 23:59
+ *
+ * Return: no return
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	h = 0;
+	while (h < 24)
+	{
+		m = 60;
+		while (m < 60)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+			m++;
+		}
+		h++;
+	}
+}
