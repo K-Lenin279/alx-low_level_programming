4e0a05832dd21722be38ae53a1eeceb16b9d56c5
K-Lenin279 <leninkagema72@gmail.com>
2023-02-22 16:52:02 +0300

    Task print n numbers to 98

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ *
+ * @a: int to be added
+ *
+ * @b: int to be added
+ *
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	return(a + b);
+}
