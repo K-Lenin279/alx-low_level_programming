b6ff73bdf630dbf3bd2c2bfcc1960dc1e3b76625
K-Lenin279 <leninkagema72@gmail.com>
2023-02-28 14:09:49 +0300

    Task reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: input pointer character
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int a, b;
+
+	a = 0;
+	while(s[a] != '\0')
+	{
+		a++;
+	}
+
+	b = a - 1;
+	for (; b >= 0; b--)
+	{
+		_putchar(s[b]);
+	}
+	_putchar('\n');
+}
