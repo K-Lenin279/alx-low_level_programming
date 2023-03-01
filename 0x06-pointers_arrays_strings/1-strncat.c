ff9762317360f2f1a1bfad270127bf27605cde3d
K-Lenin279 <leninkagema72@gmail.com>
2023-03-01 14:42:54 +0300

    Task strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ * @dest: the pointer to the destination array,
+ * where the content is to be copied
+ * @src: the string to be copied
+ * @n: amount of bytes contained in src
+ * Return: pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n)
+	{
+		dest[i] = src[j];
+		if (src[j] != '\0')
+		{
+			break;
+		}
+		j++;
+		i++;
+	}
+	return (dest);
+}
