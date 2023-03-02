b4e99833b4a98a6cf1082ba9bc2fd8cdd03b522c
K-Lenin279 <leninkagema72@gmail.com>
2023-03-02 13:03:29 +0300

    Correction strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,20 +12,19 @@ char *_strncat(char *dest, char *src, int n)
 	int i, j;
 
 	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
 	j = 0;
-	while (j < n)
+	for (; i < 1000; i++)
 	{
-		dest[i] = src[j];
-		if (src[j] != '\0')
+		if (dest[i] == '\0')
 		{
 			break;
 		}
-		i++;
 		j++;
 	}
+	for (; i < n && src[i] != '\0'; i++)
+	{
+		dest[j + i] = src[i];
+	}
+	dest[j + i] = '\0';
 	return (dest);
 }
